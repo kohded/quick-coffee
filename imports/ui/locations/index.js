0d8184a030f5c662b5d8e89bc4ffc27c051b3594
kohded <arnold@kohded.com>
2016-04-26 03:31:26 -0700

    Updated locations page with session data and Google Maps
    Update locations page to populate businesses from the businesses session
    data. Added Google Maps package.

--- imports/ui/locations/index.js
+++ imports/ui/locations/index.js
@@ -1,3 +1,44 @@
 import { Template } from 'meteor/templating';
 
+import '../../api/businesses.js'; //TEMP Only for session business object
+
 import './index.html';
+
+//Template locationsIndexMap
+GoogleMaps.load();
+
+Template.locationsIndexMap.onCreated(function() {
+  // Use the `ready` callback to interact with the map API once the map is
+  // ready.
+  GoogleMaps.ready('map', function(map) {
+    // Add a marker to the map once it's ready.
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map     : map.instance
+    });
+  });
+});
+
+Template.locationsIndexMap.helpers({
+  mapOptions: function() {
+    // Make sure the maps API has loaded.
+    if(GoogleMaps.loaded()) {
+      // Map initialization options.
+      return {
+        center: new google.maps.LatLng(47.383871, -122.235361),
+        zoom  : 8
+      };
+    }
+  }
+});
+
+//Template - locationsIndexLocation
+Template.locationsIndexLocation.onCreated(function() {
+  Session.setDefault('businesses', businesses);
+});
+
+Template.locationsIndexLocation.helpers({
+  getBusinesses: function() {
+    return Session.get('businesses');
+  }
+});
