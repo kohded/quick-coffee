313fd8b7d6a1c3dd5e832716a14648b83af2bc27
kohded <arnold@kohded.com>
2016-04-29 02:24:01 -0700

    Added users geolocation and location search autocomplete.
    Added geolocation to mark the map on load with the users current
    locations. Added geocomplete to autocomplete the form with
    addresses/locations.

--- imports/ui/locations/index.js
+++ imports/ui/locations/index.js
@@ -1,50 +1,59 @@
 import { Template } from 'meteor/templating';
 
-import '../../api/businesses.js'; //TEMP Only for session business object
+import '../../api/businesses.js';
 
 import './index.html';
 
-//Template locationsIndexMap
-GoogleMaps.load();
+//Template Search Input
+Template.locationsIndexSearchInput.onRendered(function() {
+  this.autorun(function() {
+    //Get users current locations.
+    var latLng = Geolocation.latLng();
 
-Template.locationsIndexMap.onCreated(function() {
-  // Use the `ready` callback to interact with the map API once the map is
-  // ready.
-  GoogleMaps.ready('map', function(map) {
-    // Add a marker to the map once it's ready.
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map     : map.instance
-    });
+    //Set map options.
+    if(GoogleMaps.loaded() && latLng) {
+      $("#location").geocomplete({
+        location     : new google.maps.LatLng(latLng.lat, latLng.lng),
+        map          : $(".map-container"),
+        mapOptions   : {
+          zoom       : 14,
+          scrollwheel: false
+        },
+        markerOptions: {
+          draggable: true
+        }
+      });
+    }
   });
 });
 
 Template.locationsIndexMap.helpers({
-  mapOptions: function() {
-    // Make sure the maps API has loaded.
-    if(GoogleMaps.loaded()) {
-      // Map initialization options.
-      return {
-        center: new google.maps.LatLng(47.383871, -122.235361),
-        zoom  : 8
-      };
-    }
+  //Show an error if users current geolocation doesn't exist.
+  geolocationError: function() {
+    var error = Geolocation.error();
+
+    return error && error.message;
   }
+});
+
+//Template Map
+Template.locationsIndexMap.onRendered(function() {
+  GoogleMaps.load({
+    key      : '',
+    libraries: 'places'
+  });
+});
+
+//Template - Locations List
+Template.locationsIndexLocation.onCreated(function() {
+  Session.setDefault('businesses', businesses);
 });
 
 Template.locationsIndexLocation.events({
   'click #viewMenu': function(event) {
-    //prevent the form from refreshing the page
-    //event.preventDefault();
-
     Session.set('thisBusinessId', this.businessId);
     Session.set('businessInfo', this);
   }
-});
-
-//Template - locationsIndexLocation
-Template.locationsIndexLocation.onCreated(function() {
-  Session.setDefault('businesses', businesses);
 });
 
 Template.locationsIndexLocation.helpers({
