3e2782729331331819121fa6646faf2a6cf29567
kohded <arnold@kohded.com>
2016-05-10 15:44:41 -0700

    Added dropdown in nav for logged in user
    Added dropdown in nav for logged in users to view option for account and
    logging out.

--- imports/ui/layouts/partials/nav.js
+++ imports/ui/layouts/partials/nav.js
@@ -9,10 +9,19 @@ Template.layoutsPartialsNav.onRendered(function() {
 
 Template.layoutsPartialsNav.events({
   'click .logout': function(event){
-    event.preventDefault();
-
     AccountsTemplates.logout();
+
+    //Reload page on logout so dropdown template shows. After logout, dropdown
+    // doesn't work if you log right back in without refresh.
+    //location.reload(true); //Seems slower
+    Meteor._reload.reload();
 
     Router.go('signinIndex');
   }
+});
+
+Template.layoutsPartialsNavDropdownSignedIn.onRendered(function(){
+  $(".dropdown-button").dropdown({
+    belowOrigin: true
+  });
 });
