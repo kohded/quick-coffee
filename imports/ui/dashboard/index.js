0d2101d95422ee576efc23daa99b2be597c5b2b5
kohded <arnold@kohded.com>
2016-05-13 00:45:09 -0700

    Fixed tabs on dashboard
    Fixed tabs to keep working after refresh. The wrong template was called.

--- imports/ui/dashboard/index.js
+++ imports/ui/dashboard/index.js
@@ -2,15 +2,9 @@ import { Template } from 'meteor/templating';
 
 import './index.html';
 
-Template.dashboardIndex.onRendered(function() {
-  $(document).ready(function() {
-    $('ul.tabs').tabs();
-  });
-  $(document).ready(function() {
-    $('.collapsible').collapsible({
-      accordion: true
-    });
-  });
+Template.dashboardIndexTabs.onRendered(function() {
+  //Load tabs
+  $('ul.tabs').tabs();
 });
 
 Template.dashboardIndexAccount.events({
