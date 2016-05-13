ba292264ea61a4510be96b4381a22acca7827641
kohded <arnold@kohded.com>
2016-05-12 19:27:56 -0700

    Users can update their account information
    Users can update their username, email, and password within the
    dashboard.

--- imports/api/users/users.js
+++ imports/api/users/users.js
@@ -1,12 +1,20 @@
 import { Meteor } from 'meteor/meteor';
 
-
 Meteor.methods({
   //Add business role when user registers business.
   'assignBusinessRole'(userId) {
     var role = 'business';
 
     Roles.addUsersToRoles(userId, role);
+  },
+  'updateUsername'(userId, username){
+    Meteor.users.update({_id: userId}, {$set: {username: username}});
+  },
+  'updateEmail'(userId, email){
+    Meteor.users.update({_id: userId}, {$set: {email: email}});
+  },
+  'updatePassword'(userId, password){
+    Accounts.setPassword(userId, password, {logout: false});
   }
 });
 
--- imports/ui/dashboard/index.js
+++ imports/ui/dashboard/index.js
@@ -6,4 +6,45 @@ Template.dashboardIndex.onRendered(function() {
   $(document).ready(function() {
     $('ul.tabs').tabs();
   });
+  $(document).ready(function() {
+    $('.collapsible').collapsible({
+      accordion: true
+    });
+  });
+});
+
+Template.dashboardIndexAccount.events({
+  'submit #update-username': function(event) {
+    event.preventDefault();
+
+    var username = $('#username').val();
+
+    //Update username
+    Meteor.call('updateUsername', Meteor.userId(), username);
+
+    //Clear form values
+    event.target.reset();
+  },
+  'submit #update-email'   : function(event) {
+    event.preventDefault();
+
+    var email = $('#email').val();
+
+    //Update email
+    Meteor.call('updateEmail', Meteor.userId(), email);
+
+    //Clear form values
+    event.target.reset();
+  },
+  'submit #update-password': function(event) {
+    event.preventDefault();
+
+    var password = $('#password').val();
+
+    //Update password
+    Meteor.call('updatePassword', Meteor.userId(), password);
+
+    //Clear form values
+    event.target.reset();
+  }
 });
